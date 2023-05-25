69b723a2d639c9f7a5e08b754b7b9033f09169c1
AsrianaFebriansyah02 <124341045+AsrianaFebriansyah02@users.noreply.github.com>
2023-05-25 08:29:10 +0700

    INI STUDI KASUS

--- BackEnd/index.php
+++ BackEnd/index.php
@@ -12,6 +12,36 @@
 </head>
 
 <body>
+    <!-- awal navbar -->
+    <nav class="navbar navbar-dark bg-primary navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Admin Panel Dashboard
+
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Setting</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-dark" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!-- akhir navbar -->
+
+
+    <!-- awal tabel -->
     <div class="container card mt-5">
         <div class="card-body ">
             <a href="tambah.php" Class="btn btn-primary mt-3 mb-3">Tambah Data</a>
@@ -67,6 +97,7 @@
             $('#example').DataTable();
         });
     </script>
+    <!-- akhir tabel -->
 </body>
 
 </html>
